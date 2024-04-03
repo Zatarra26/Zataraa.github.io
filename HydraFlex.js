7cd2d69af48b53571ee423f8ea5e7ce18fd077a8
Zatarra26 <165933347+Zatarra26@users.noreply.github.com>
2024-04-04 00:16:49 +0800

    Add files via upload

---
+++ HydraFlex.js
@@ -0,0 +1,20 @@
+document.getElementById("mission").addEventListener("mouseover", function() {
+    document.getElementById("mission_text").style.opacity = "1";
+});
+document.getElementById("mission").addEventListener("mouseout", function() {
+    document.getElementById("mission_text").style.opacity = "0";
+});
+
+document.getElementById("vision").addEventListener("mouseover", function() {
+    document.getElementById("vision_text").style.opacity = "1";
+});
+document.getElementById("vision").addEventListener("mouseout", function() {
+    document.getElementById("vision_text").style.opacity = "0";
+});
+
+document.getElementById("values").addEventListener("mouseover", function() {
+    document.getElementById("values_text").style.opacity = "1";
+});
+document.getElementById("values").addEventListener("mouseout", function() {
+    document.getElementById("values_text").style.opacity = "0";
+});
